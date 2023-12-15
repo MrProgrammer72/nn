1ad20c3450e4732cd84d8e63586da3d0e2af7c67
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-15 21:38:42 +0530

    Update help.py

--- SankiMusic/utilities/inline/help.py
+++ SankiMusic/utilities/inline/help.py
@@ -16,7 +16,7 @@ def help_pannel(_, START: Union[bool, int] = None):
         ),
         InlineKeyboardButton(
             text="ᴍᴏʀᴇ",
-            url=f"t.me/lippxd",
+            url=f"t.me/lippsxd",
         ),
         InlineKeyboardButton(
             text="ᴄʟᴏsᴇ", callback_data=f"close"
