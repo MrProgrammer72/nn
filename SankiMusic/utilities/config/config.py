0ab17fae548addfb305cecc51570c943fe2cd143
ᴍʀ.ᴘʀᴏɢᴀᴍᴍᴇʀ <109902449+MrProgrammer72@users.noreply.github.com>
2023-12-15 18:41:34 +0530

    Add files via upload

---
+++ SankiMusic/utilities/config/config.py
@@ -0,0 +1,111 @@
+import os
+import re
+import sys
+
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import filters
+
+if os.path.exists("Internal"):
+  load_dotenv("Internal")
+
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
+BOT_TOKEN = getenv("BOT_TOKEN")
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+MONGO_DB_URL = getenv("MONGO_DB_URL", None)
+LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
+MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Kritka Music")
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5388906652").split()))
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/MrProgrammer72/n")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "kaal")
+GIT_TOKEN = getenv("GIT_TOKEN", None)
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/GJ516_DISCUSS_GROUP")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/GJ516_DISCUSS_GROUP")
+SUPPORT_HEHE = SUPPORT_GROUP.split("me/")[1]
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", "180"))
+SONG_DOWNLOAD_DURATION = int(getenv("SONG_DOWNLOAD_DURATION_LIMIT", "180"))
+AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "Flase")
+AUTO_LEAVE_ASSISTANT_TIME = int(getenv("ASSISTANT_LEAVE_TIME", "5400"))
+AUTO_DOWNLOADS_CLEAR = getenv("AUTO_DOWNLOADS_CLEAR", "True")
+PRIVATE_BOT_MODE = getenv("PRIVATE_BOT_MODE", None)
+YOUTUBE_DOWNLOAD_EDIT_SLEEP = int(getenv("YOUTUBE_EDIT_SLEEP", "5"))
+TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "3"))
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+VIDEO_STREAM_LIMIT = int(getenv("VIDEO_STREAM_LIMIT", "5"))
+SERVER_PLAYLIST_LIMIT = int(getenv("SERVER_PLAYLIST_LIMIT", "50"))
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", "50"))
+CLEANMODE_DELETE_MINS = int(getenv("CLEANMODE_MINS", "12"))
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", "104857600"))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", "1073741824"))
+# https://www.gbmb.org/mb-to-bytes
+
+STRING1 = getenv("STRING_SESSION", None)
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+############################
+COMMAND_PREFIXES.append('')
+############################
+BANNED_USERS = filters.user()
+YTDOWNLOADER = 1
+LOG = 2
+LOG_FILE_NAME = "sankilogs.txt"
+adminlist = {}
+lyrical = {}
+chatstats = {}
+userstats = {}
+clean = {}
+autoclean = []
+############################
+
+START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/5e18f8ea275b70df0f3a1.jpg")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/cc6c9d8726a107961b34f.jpg")
+
+PLAYLIST_IMG_URL = "https://telegra.ph/file/ba7a1f81baea95f0c0883.jpg"
+GLOBAL_IMG_URL = "https://te.legra.ph/file/a4baaf3b501b8de67ebc9.jpg"
+STATS_IMG_URL = "https://te.legra.ph/file/a4baaf3b501b8de67ebc9.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org/file/7115db868dca83982d847.jpg"
+TELEGRAM_VIDEO_URL = "https://graph.org/file/d1d3c2a17035d9003271e.jpg"
+STREAM_IMG_URL = "https://graph.org/file/8a40115f892539d9ee41c.jpg"
+SOUNCLOUD_IMG_URL = "https://graph.org/file/d1d3c2a17035d9003271e.jpg"
+YOUTUBE_IMG_URL = "https://graph.org/file/498a24039b7f66568edf1.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/bd72db7afa8a08a916d49.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(
+        int(x) * 60**i
+        for i, x in enumerate(reversed(stringt.split(":")))
+    )
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+SONG_DOWNLOAD_DURATION_LIMIT = int(
+    time_to_seconds(f"{SONG_DOWNLOAD_DURATION}:00")
+)
+
+if UPSTREAM_REPO:
+    if not re.match("(?:http|https)://", UPSTREAM_REPO):
+        print(
+            "[ERROR] - Your UPSTREAM_REPO url is wrong. Please ensure that it starts with https://"
+        )
+        sys.exit()
+
+if PING_IMG_URL:
+    if PING_IMG_URL != "SankiMusic/resource/Ping.jpeg":
+        if not re.match("(?:http|https)://", PING_IMG_URL):
+            PING_IMG_URL = "https://te.legra.ph/file/cc6c9d8726a107961b34f.jpg"
+
+if START_IMG_URL:
+    if START_IMG_URL != "SankiMusic/resource/Ping.jpeg":
+        if not re.match("(?:http|https)://", START_IMG_URL):
+            START_IMG_URL = "https://te.legra.ph/file/5e18f8ea275b70df0f3a1.jpg"
